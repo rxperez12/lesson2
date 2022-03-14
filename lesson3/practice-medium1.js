59c7ebface7ae8024a250d46d3cb5e7cf8f2b030
Ricardo Perez <rxperez12@gmail.com>
2022-03-14 16:35:55 -0700

    practice-hard1.js

--- lesson3/practice-medium1.js
+++ lesson3/practice-medium1.js
@@ -59,14 +59,14 @@
 // }
 
 //question 9
-function rps(fist1, fist2) {
-  if (fist1 === "rock") {
-    return fist2 === "paper" ? "paper" : "rock";
-  } else if (fist1 === "paper") {
-    return fist2 === "scissors" ? "scissors" : "paper";
-  } else {
-    return fist2 === "rock" ? "rock" : "scissors";
-  }
-}
-console.log(rps("rock", "scissors"));
-console.log(rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock"));
+// function rps(fist1, fist2) {
+//   if (fist1 === "rock") {
+//     return fist2 === "paper" ? "paper" : "rock";
+//   } else if (fist1 === "paper") {
+//     return fist2 === "scissors" ? "scissors" : "paper";
+//   } else {
+//     return fist2 === "rock" ? "rock" : "scissors";
+//   }
+// }
+// console.log(rps("rock", "scissors"));
+// console.log(rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock"));
