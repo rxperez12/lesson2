9b90ce433d148583fb585db697d8b5b2c19800d4
Ricardo Perez <rxperez12@gmail.com>
2022-03-14 15:09:31 -0700

    practice-medium1.js

---
+++ lesson3/practice-easy3.js
@@ -0,0 +1,25 @@
+//question 1
+// let numbers = [1, 2, 3, 4];
+// numbers.length = 0;
+// numbers.splice(0, numbers.length);
+
+// while (numbers.length) {
+//   numbers.pop();
+// }
+
+// question 2
+// + sign doesn't concat two arrays in javascript
+// makes them into strings and then adds those
+
+// question 5
+// function isColorValid(color) {
+//   if (color === "blue" || color === "green") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function isColorValid(color) {
+//   return color === "blue" || color === "green";
+// }
---
+++ lesson3/practice-medium1.js
@@ -0,0 +1,72 @@
+// question 1
+// let phrase = "The Flinstones Rock!";
+// for (let i = 0; i < 10; i++) {
+//   phrase = " " + phrase;
+//   console.log(phrase);
+// }
+// for (let padding = 1; padding <= 10; padding++) {
+//   console.log(" ".repeat(padding) + "The Flinstones Rock!");
+// }
+
+// question 2
+// let munstersDescription = "The Munsters are creepy and spooky.";
+// let result = "";
+// for (let i = 0; i < munstersDescription.length; i++) {
+//   if (munstersDescription[i] === munstersDescription[i].toLowerCase()) {
+//     result += munstersDescription[i].toUpperCase();
+//   } else {
+//     result += munstersDescription[i].toLowerCase();
+//   }
+// }
+// console.log(result);
+
+// munstersDescription.split("").map(function(char) {
+//   if (char === char.toUpperCase()) {
+//     return char.toLowerCase();
+//   } else {
+//     return char.toUpperCase();
+//   }
+// }).join("");
+
+//question 3
+// function factors(number) {
+//   let divisor = number;
+//   let factors = [];
+//   while (divisor > 0) {
+//     if (number % divisor === 0) {
+//       factors.push(number / divisor);
+//     }
+//     divisor--;
+//   }
+//   return factors;
+// }
+// console.log(factors(10));
+
+// question 8
+// let munsters = {
+//   Herman: { age: 32, gender: "male" },
+//   Lily: { age: 30, gender: "female" },
+//   Grandpa: { age: 402, gender: "male" },
+//   Eddie: { age: 10, gender: "male" },
+//   Marilyn: { age: 23, gender: "female" },
+// };
+
+// function messWithDemographics(demoObject) {
+//   Object.values(demoObject).forEach((familyMember) => {
+//     familyMember["age"] += 42;
+//     familyMember["gender"] = "other";
+//   });
+// }
+
+//question 9
+function rps(fist1, fist2) {
+  if (fist1 === "rock") {
+    return fist2 === "paper" ? "paper" : "rock";
+  } else if (fist1 === "paper") {
+    return fist2 === "scissors" ? "scissors" : "paper";
+  } else {
+    return fist2 === "rock" ? "rock" : "scissors";
+  }
+}
+console.log(rps("rock", "scissors"));
+console.log(rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock"));
